410a7820f6cfff6b3055b4c4b1fbd4a33939d016
12345EmmaAgyei <2425402769@live.gctu.edu.gh>
2026-05-31 00:04:52 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: seantherapper.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Emmanuel Agyei
+# STUDENT ID: 2425402769
+# Email: beyondsee4@gmail.com
+# Grader: Augustus
+#
+# On my honor, Emmanuel Agyei, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+# my initials print
 
+    print()
+    print("...EA")
+    print()
+    print("...EEEEEEEEEEEE...........AAAAAA........")
+    print("...EEEEEEEEEEEE..........AAA..AAA.......")
+    print("...EEE..................AAA....AAA......")
+    print("...EEE.................AAA......AAA.....")
+    print("...EEEEEEEEEEEE........AAAAAAAAAAAA.....")
+    print("...EEEEEEEEEEEE........AAAAAAAAAAAA.....")
+    print("...EEE.................AAA......AAA.....")
+    print("...EEE.................AAA......AAA.....")
+    print("...EEEEEEEEEEEE...**...AAA......AAA...**")
+    print("...EEEEEEEEEEEE...**...AAA......AAA...**")
+    print()
 
 
 main()
+
